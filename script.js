95cdb4c98b0c7af9c8fa85fe6f499a5b6418d401
Leonbirch <leonbirch333@gmail.com>
2024-01-31 15:43:39 +0000

    added defer to all script tags

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 
 
+/*this selects where i want the data to be displayed*/
 
-
-
+const projectTarget = document.querySelector('.portfolio')
 
 
 fetch ('projects.json').then(function (response) {
     return response.json()
 
-}).then
-(function (data) {
+}).then (function (data) {
    console.log(data)
+
+   data.projects.forEach(function (project) {
+    console.log(project)
+
+    projectTarget.innerHTML += `<div>
+    <h3>${project.name}</h3>
+    <img src='${project.img}'/>
+    <p>${project.desc}</p>
+    </div>`
+
+
+   })
 })
-
-
-
-
-
 
